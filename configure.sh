645d3302b68a5231e6ebe8dbdc68d8a54d64c31d
paralleler0 <paralleler0@gmail.com>
2026-05-27 13:21:14 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -27,8 +27,11 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
 # 6. Install ngrok using the correct modern formula/cask structurebrew install ngrok
-brew tap ngrok/ngrok
-brew install ngrok
+curl -sSOL https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-darwin-amd64.zip
+unzip -o ngrok-v3-stable-darwin-amd64.zip
+sudo mv ngrok /usr/local/bin/
+
+rm ngrok-v3-stable-darwin-amd64.zip
 # 7. Configure ngrok and start it
 ngrok authtoken $3
 ngrok tcp 5900 &
