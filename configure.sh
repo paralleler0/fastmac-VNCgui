114a48791929c22869212a0fa05fe9c0a90ab700
paralleler0 <paralleler0@gmail.com>
2026-05-27 16:46:45 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# Usage: source configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+# Usage: ./configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
 
-# 1. Disable spotlight indexing
+# 1. Disable spotlight
 sudo mdutil -i off -a
 
-# 2. Create new account safely
+# 2. Create new account 
+cd /tmp
 sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
 sudo dscl . -create /Users/vncuser RealName "VNC User"
 sudo dscl . -create /Users/vncuser UniqueID 1001
 sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-# Fixed the duplicate password pipe by passing it cleanly via stdin
 echo "$1" | sudo dscl . -passwd /Users/vncuser
+sudo dseditgroup -o edit -a vncuser -t user admin
 sudo createhomedir -c -u vncuser > /dev/null
 
 # 3. Enable VNC
@@ -20,19 +21,19 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes
 
 # 4. Set VNC password
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+echo "$2" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt > /dev/null
 
-# 5. Start VNC/reset changes
+# 5. Start VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-# 6. Install ngrok 
-curl -Lk https://bin.equinox.io/c/b34236/ngrok-v3-stable-darwin-amd64.zip -o ngrok.zip
+# 6. Install Ngrok 
+curl -Lk https://bin.equinox.io/c/b34236/ngrok-v3-stable-darwin-arm64.zip -o ngrok.zip
 unzip -o ngrok.zip
 chmod +x ./ngrok
 sudo mv ./ngrok /usr/local/bin/ngrok
 rm ngrok.zip
 
-# 7. Configure ngrok and start it
-ngrok authtoken $3
-ngrok tcp 5900 &
+# 7. Configure and Start
+/usr/local/bin/ngrok authtoken "$3"
+/usr/local/bin/ngrok tcp 5900 --log=stdout &
