e11e48d5e1c74edfbbfe26c41729ccee16e72f68
paralleler0 <paralleler0@gmail.com>
2026-05-27 13:05:20 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,33 +1,34 @@
-#configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
+#!/bin/bash
+# Usage: source configure.sh VNC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN
 
-#disable spotlight indexing
+# 1. Disable spotlight indexing
 sudo mdutil -i off -a
 
-#Create new account
+# 2. Create new account safely
 sudo dscl . -create /Users/vncuser
 sudo dscl . -create /Users/vncuser UserShell /bin/bash
 sudo dscl . -create /Users/vncuser RealName "VNC User"
 sudo dscl . -create /Users/vncuser UniqueID 1001
 sudo dscl . -create /Users/vncuser PrimaryGroupID 80
 sudo dscl . -create /Users/vncuser NFSHomeDirectory /Users/vncuser
-sudo dscl . -passwd /Users/vncuser $1
-sudo dscl . -passwd /Users/vncuser $1
+# Fixed the duplicate password pipe by passing it cleanly via stdin
+echo "$1" | sudo dscl . -passwd /Users/vncuser
 sudo createhomedir -c -u vncuser > /dev/null
 
-#Enable VNC
+# 3. Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
 
-#VNC password - http://hints.macworld.com/article.php?story=20071103011608872
+# 4. Set VNC password
 echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# 5. Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
+# 6. Install ngrok using the correct modern formula/cask structure
 brew install --cask ngrok
 
-#configure ngrok and start it
+# 7. Configure ngrok and start it
 ngrok authtoken $3
 ngrok tcp 5900 &
